68a831802621ecf8d2349527aca796bec466e946
RCO8 <gurwo63@naver.com>
2026-09-08 20:11:02 +0900

    Docker,My SQL 환경변수 세팅

--- src/main/java/com/gamebasic/GameBasicApplication.java
+++ src/main/java/com/gamebasic/GameBasicApplication.java
@@ -2,8 +2,11 @@ package com.gamebasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "com.gamebasic.game.repository")
 public class GameBasicApplication {
 
     public static void main(String[] args) {
