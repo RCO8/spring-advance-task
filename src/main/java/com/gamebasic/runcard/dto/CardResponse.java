7597d0b4307d8ef89f2df423c5c61e3e8d58e64a
RCO8 <gurwo63@naver.com>
2026-09-09 16:13:32 +0900

    CardResponse final속성 추가

--- src/main/java/com/gamebasic/runcard/dto/CardResponse.java
+++ src/main/java/com/gamebasic/runcard/dto/CardResponse.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 @Getter
 public class CardResponse {
     // TODO (Lv 5): API 명세의 카드 응답 JSON에 맞게 필드를 만들고 생성자에서 채우세요.
-    private Long id;
-    private String cardType;
-    private Integer acquiredFloor;
+    private final Long id;
+    private final String cardType;
+    private final Integer acquiredFloor;
 
     public CardResponse(Long id, String cardType, int acquiredFloor) {
         this.id = id;
