7982db5822099bcf902fba0d46d66db335466ebb
RCO8 <gurwo63@naver.com>
2026-09-11 10:41:14 +0900

    createAt, updateAt추가

--- src/main/java/com/gamebasic/game/entity/Game.java
+++ src/main/java/com/gamebasic/game/entity/Game.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "games")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Game {
+public class Game extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,6 +37,9 @@ public class Game {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 16)
     private GameStatus status;
+
+    @Column(nullable = false)
+    private int deckSize;
 
     public Game(String playerName) {
         this.playerName = playerName;
